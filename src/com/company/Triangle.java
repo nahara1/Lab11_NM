6a7e273b23c94594e7ac0123722ad9545495457d
nahara1 <narr.melo@gmail.com>
2019-03-30 16:16:36 -0400

    update

--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -11,7 +11,7 @@ public class Triangle extends Shape {
 
     public Triangle(Point p1, Point p2, Point p3) {
 
-        System.out.println("Triangle Points: (X:" + p1.x + ", Y:" + p1.y + "), " + "(X" + p2.x + ", Y:" + p2.y + "), " + "(X:" + p3.x + ", Y:" + p3.y + ")\n");
+        System.out.println("Triangle Points: (X:" + p1.x + ", Y:" + p1.y + "), " + "(X:" + p2.x + ", Y:" + p2.y + "), " + "(X:" + p3.x + ", Y:" + p3.y + ")\n");
 
     }
 }
